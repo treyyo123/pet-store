5f076d1c3fadc4379a91be962923bd2a62d90e64
trey baum <treybaum2010@gmail.com>
2024-02-22 20:26:08 -0500

    Week 13 commit

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ */
+@SpringBootApplication
+public class PetStoreApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PetStoreApplication.class, args);		
+	}
+
+}
