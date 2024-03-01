1918e2a0f46058d8d647277f6d9de2feced85ded
trey baum <treybaum2010@gmail.com>
2024-02-29 22:23:48 -0500

    Week 14 first commit

---
+++ src/main/java/pet/store/controller/model/PetStoreEmployee.java
@@ -0,0 +1,19 @@
+package pet.store.controller.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pet.store.entity.Employee;
+
+@Data
+@NoArgsConstructor
+public class PetStoreEmployee {
+	private Long employeeId;
+	private String employeeFirstName;
+	private String employeeLastName;
+	private String employeePhone;
+	private String employeeJobTitle;
+	
+	PetStoreEmployee(Employee employee){
+		
+	}
+}
